64d208eb2b43ad67fd0e9aeedc3a4fc387916a33
Mcontre11 <78456343+Mcontre11@users.noreply.github.com>
2021-10-02 00:25:45 -0400

    commit

---
+++ main.js
@@ -0,0 +1,43 @@
+document.querySelector("#button").addEventListener("click", nasa)
+
+function nasa() {
+  let date = document.querySelector('input')
+  let htmlImage = document.querySelector('#nasaImage')
+  let explanation = document.querySelector('#infoFromNasa')
+  let nasaPageTitle = document.querySelector('#title')
+  let video = document.querySelector('#nasaVideo')
+  
+  const url = ('https://api.nasa.gov/planetary/apod?api_key= + date.value)
+
+
+  fetch(url)
+    .then(res => res.json()) // parse response as JSON 
+    .then(dataFromNasa => {
+      // console.log('this is the dataFromNasa variable;', dataFromNasa);
+      // console.log('this is the dataFromNasa.code variable;', dataFromNasa.code);
+      if (dataFromNasa.code == 400) {
+        let errorMessage = 'ERROR   ' + dataFromNasa.msg
+        alert(errorMessage)
+        // else is changingn the html attribute to the dataFromNasa given by nasa
+      } else if(dataFromNasa.media_type=='video'){
+        video.src=dataFromNasa.url
+        htmlImage.classList.add('hide')
+        explanation.innerText = dataFromNasa.explanation
+        nasaPageTitle.innerText = dataFromNasa.title
+        video.classList.remove('hide')
+      }
+      else {
+        htmlImage.src=dataFromNasa.url
+        video.classList.add('hide')
+        explanation.innerText = dataFromNasa.explanation
+        nasaPageTitle.innerText = dataFromNasa.title
+        htmlImage.classList.remove('hide')
+
+      }
+    })
+
+  
+     .catch(err => {
+       console.log(`error ${err}`);
+     });
+  }
